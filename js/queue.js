77a398775971a1519728f91cc8d3f8ea099d6c53
Ryan Whitmore <ryan@whitmorespace.com>
2020-09-25 07:42:24 -0700

    first

---
+++ js/queue.js
@@ -0,0 +1,28 @@
+// Creates a node containing the data and a reference to the next item
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      this.last.next = node;
+    }
+    //make the new node the last item on the queue
+    this.last = node;
+  }
+}
